f4ed65e52024ac1f3c3fc4929dbfa4a1b2996f50
dheerain-jain <jain.dheerain@gmail.com>
2017-07-30 19:44:44 +0530

    CheckAdded

--- app/src/main/java/dheerain/jain/zhilmil/MainActivity.java
+++ app/src/main/java/dheerain/jain/zhilmil/MainActivity.java
@@ -1,16 +1,63 @@
 package dheerain.jain.zhilmil;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView buttonSwitch;
+    View v;
+    Camera camera;
+    private boolean isFlashOn,hasFlash;
+    private Camera.Parameters parameters;
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        v=findViewById(android.R.id.content);
+        checkFlash();
+
+
         
+    }
+
+    void checkFlash(){
+        if(getApplicationContext().getPackageManager()
+                .hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)){
+
+            Snackbar.make(v, "You have flash", Snackbar.LENGTH_LONG)
+                    .setAction("CLOSE", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+
+                        }
+                    })
+                    .setActionTextColor(getResources().getColor(android.R.color.holo_red_light ))
+                    .show();
+
+        }
+        else {
+
+            Snackbar.make(v, "You Dont have flash", Snackbar.LENGTH_LONG)
+                    .setAction("CLOSE", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                                finish();
+                        }
+                    })
+                    .setActionTextColor(getResources().getColor(android.R.color.holo_red_light ))
+                    .show();
+
+        }
 
     }
 }
