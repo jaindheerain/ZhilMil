5a50d6b570dfd10402b891b5f6df43959406f952
dheerain-jain <jain.dheerain@gmail.com>
2017-07-30 21:43:39 +0530

    BlinkingAdded;AychAdded

--- app/src/main/java/dheerain/jain/zhilmil/MainActivity.java
+++ app/src/main/java/dheerain/jain/zhilmil/MainActivity.java
@@ -2,21 +2,25 @@ package dheerain.jain.zhilmil;
 
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
-import android.media.MediaPlayer;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView buttonSwitch;
+    ImageView powerButton;
     View v;
     Camera camera;
-    private boolean isFlashOn,hasFlash;
+    public ticker a=new ticker();
+    boolean isClicked=false;
+    public int delay=100;
+    private boolean isFlashOn=false;
     private Camera.Parameters parameters;
-    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +29,27 @@ public class MainActivity extends AppCompatActivity {
 
         v=findViewById(android.R.id.content);
         checkFlash();
+        powerButton= (ImageView) findViewById(R.id.switchButton);
+        getCamera();
+        switchButton();
+        powerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isFlashOn || isClicked){
 
+                    Toast.makeText(MainActivity.this, "ON", Toast.LENGTH_SHORT).show();
+                    a.cancel(true);
+                    isClicked=false;
+                }
+                else {
+                    a=new ticker();
+                    a.execute();
+                    isClicked=true;
+                    Toast.makeText(MainActivity.this, "OFF", Toast.LENGTH_SHORT).show();
 
+                }
+            }
+        });
 
     }
 
@@ -47,6 +70,139 @@ public class MainActivity extends AppCompatActivity {
                     .show();
             finish();
 
+        }
+    }
+
+    void getCamera() {
+        if (camera == null) {
+            try {
+                camera = Camera.open();
+                parameters = camera.getParameters();
+            } catch (RuntimeException e) {
+                Log.e("Error Camera",e.getMessage());
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(isFlashOn)
+            turnOn();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        turnOff();
+        camera.release();
+    }
+
+    void turnOn() {
+        if (!isFlashOn) {
+            if (camera == null || parameters == null) {
+                return;
+            }
+            parameters = camera.getParameters();
+            parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+            camera.setParameters(parameters);
+            camera.startPreview();
+            isFlashOn = true;
+//            switchButton();
+        }
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        getCamera();
+    }
+
+    void turnOff() {
+        if (isFlashOn) {
+            if (camera == null || parameters == null) {
+                return;
+            }
+            parameters = camera.getParameters();
+            parameters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+            camera.setParameters(parameters);
+            camera.stopPreview();
+            isFlashOn = false;
+  //          switchButton();
+        }
+    }
+
+    void switchButton(){
+        if(isFlashOn){
+            powerButton.setImageResource(R.mipmap.ic_launcher);
+        }else{
+            powerButton.setImageResource(R.mipmap.ic_launcher_round);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (camera != null) {
+            camera.release();
+            camera = null;
+        }
+    }
+
+    public void toggleFlashLight() {
+        if (!isFlashOn) { // Off, turn it on
+            turnOn();
+        } else { // On, turn it off
+            turnOff();
+        }
+    }
+
+
+    Thread t=new Thread(new Runnable() {
+        @Override
+        public void run() {
+            for (int i=0; i < 50; i++) {
+                toggleFlashLight();
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    });
+
+    public  class ticker extends AsyncTask<Void,Void,Void>{
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            a=new ticker();
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            //t.run();
+            for (int i=0; i <i*2; i++) {
+                if(isCancelled())break;
+                toggleFlashLight();
+                try {
+                    Thread.sleep(delay);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
+            if(isFlashOn)
+                toggleFlashLight();
         }
     }
 }
---
+++ app/src/main/java/dheerain/jain/zhilmil/frequency.java
@@ -0,0 +1,8 @@
+package dheerain.jain.zhilmil;
+
+/**
+ * Created by Dheerain on 30-07-2017.
+ */
+
+public class frequency {
+}
